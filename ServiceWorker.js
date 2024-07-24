46f8b50d16ce31d80b6fa94236b9fedd630bb76f
zxm0345 <zxm0345@sina.com>
2024-07-24 23:37:39 +0800

    1

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "HLJHEU-齿轮虚拟加工和检测实验系统-1.0.0.1";
+const contentToCache = [
+    "Build/01.loader.js",
+    "Build/01.framework.js.unityweb",
+    "Build/01.data.unityweb",
+    "Build/01.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
